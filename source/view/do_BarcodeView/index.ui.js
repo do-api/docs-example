90cd10851103f93b695d37aab03a7a651a0733be
jiaxb <1490521037@qq.com>
2017-08-25 17:39:49 +0800

    do_BarcodeView组件

---
+++ source/view/do_BarcodeView/index.ui.js
@@ -0,0 +1,29 @@
+var do_BarcodeView = ui("do_BarcodeView_1");
+
+sm("do_Page").on("back",function(){
+	sm("do_App").closePage();
+});
+
+sm("do_Page").on("loaded",function(){
+	//启动扫描
+	do_BarcodeView.start(function(data) {
+		if (data) deviceone.print(JSON.stringify(data),"扫描结果");
+	});
+})
+
+//设置扫描的区域 150,150,300,300
+ui("do_Button_setscanarea").on("touch",function(){
+	do_BarcodeView.scanArea = "150,150,300,300";
+//	do_BarcodeView.start(function(data) {
+//		if (data) deviceone.print(JSON.stringify(data),"扫描结果");
+//	});
+});
+
+//开启闪光灯
+ui("do_Button_on").on("touch",function(){
+	do_BarcodeView.flash("on");
+});
+//关闭闪光灯
+ui("do_Button_off").on("touch",function(){
+	do_BarcodeView.flash("off");
+});
