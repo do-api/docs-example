519f726cb9f7e248959f48d18659696d4a99eac9
jiaxb <1490521037@qq.com>
2017-08-25 18:07:44 +0800

    do_BarcodeView更新

--- source/view/do_BarcodeView/index.ui.js
+++ source/view/do_BarcodeView/index.ui.js
@@ -11,15 +11,16 @@ sm("do_Page").on("loaded",function(){
 	});
 })
 
-//设置扫描的区域 150,150,300,300
+//设置扫描的区域 150,150,300,300,设置后需要调用start方法重新启动扫描才可以生效
 ui("do_Button_setscanarea").on("touch",function(){
 	do_BarcodeView.scanArea = "150,150,300,300";
-//	do_BarcodeView.start(function(data) {
-//		if (data) deviceone.print(JSON.stringify(data),"扫描结果");
-//	});
-});
+	do_BarcodeView.start(function(data) {
+		if (data)
+			deviceone.print(JSON.stringify(data), "扫描结果");
+	});
+})
 
-//开启闪光灯
+// 开启闪光灯
 ui("do_Button_on").on("touch",function(){
 	do_BarcodeView.flash("on");
 });
