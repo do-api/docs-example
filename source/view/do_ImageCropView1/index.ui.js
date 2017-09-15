bb7ce14b8e6a87a64c3560303b2e29128bdb779e
yangy <1075414853@qq.com>
2017-09-15 15:47:07 +0800

    do_ImageCropView1

---
+++ source/view/do_ImageCropView1/index.ui.js
@@ -0,0 +1,25 @@
+//related to crop.ui
+
+ui("do_ALayout_back").on("touch", function() {
+	sm("do_App").closePage();
+});
+sm("do_Page").on("back", function() {
+	sm("do_App").closePage();
+});
+
+var do_ImageCropView = ui("do_ImageCropView1_1");
+
+
+//1.属性----------------------------------------------------
+//source属性
+do_ImageCropView.source = "source://view/do_ImageCropView/image/1.jpg";
+
+
+//2.方法----------------------------------------------------
+//crop方法
+ui("do_ALayout_ok").on("touch",function(){
+	do_ImageCropView.crop(function(data, e) {
+		deviceone.print(data);
+	})
+})
+
