737973c1cf9db0585b80c240f3a74645d0cee7cf
yangy <1075414853@qq.com>
2017-09-15 17:32:32 +0800

    IconFont

---
+++ source/view/do_IconFont/index.ui.js
@@ -0,0 +1,20 @@
+//实例化
+var do_IconFont = ui("do_IconFont_1");
+
+//属性-----------------------------
+
+//iconColor字体颜色
+do_IconFont.iconColor = "FF0000FF";
+
+//iconSize字体大小
+do_IconFont.iconSize = 300;
+
+//iconName字体名称
+do_IconFont.iconName = "IconFont";
+
+//iconColor字库对应图标代码
+do_IconFont.iconCode = "e6e0";
+
+do_IconFont.on("touch",function(data){
+	
+})
