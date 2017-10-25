05ebdf35944cb71f25a3eb99169fd6e3b9609c15
yangy <1075414853@qq.com>
2017-10-25 10:44:08 +0800

    do_SeekBar

---
+++ source/view/do_SeekBar/index.ui.js
@@ -0,0 +1,15 @@
+ui("do_ALayout_back").on("touch", function() {
+	sm("do_App").closePage();
+})
+sm("do_Page").on("back", function() {
+	sm("do_App").closePage();
+});
+
+var do_SeekBar_1 = ui("do_SeekBar_1");
+do_SeekBar_1.progress = 50;
+
+do_SeekBar_1.secondaryProgress = 90;
+
+do_SeekBar_1.on("progressChanged",function(data){
+	deviceone.print(data)
+})
